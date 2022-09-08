6a91f35af412b858e5ba3b5b58bf99a1c5241919
Stiven Masha <stiven.masha@satispay.com>
2022-09-08 17:02:40 +0200

    Add script to generate marketplace zip package

---
+++ scripts/zip-for-marketplace.sh
@@ -0,0 +1,12 @@
+mkdir -p ~/Downloads/Satispay
+cp -R docs ~/Downloads/Satispay
+cp -R src ~/Downloads/Satispay
+cp -R vendor ~/Downloads/Satispay
+cp -R .eslintignore ~/Downloads/Satispay
+cp -R .gitignore ~/Downloads/Satispay
+cp -R composer.json ~/Downloads/Satispay
+cp -R composer.lock ~/Downloads/Satispay
+cp -R LICENSE ~/Downloads/Satispay
+cp -R README.md ~/Downloads/Satispay
+cp -R RELEASENOTES.md ~/Downloads/Satispay
+cd ~/Downloads && zip -r Satispay.zip Satispay
