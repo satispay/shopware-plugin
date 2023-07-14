99eb8cfe424e06e573ba21eb6840a189396d084d
Frankie <francesco.chiaramonte@satispay.com>
2023-07-14 15:34:04 +0200

    Fixed "zip-for-marketplace.sh" script

--- zip-for-marketplace.sh
+++ zip-for-marketplace.sh
@@ -1,5 +1,9 @@
+mkdir -p ~/Downloads/Satispay
 cp -R LICENSE ~/Downloads/Satispay
 cp -R README.md ~/Downloads/Satispay
 cp -R RELEASENOTES.md ~/Downloads/Satispay
+cp -R docs ~/Downloads/Satispay
+cp -R src ~/Downloads/Satispay
+cp -R composer.json ~/Downloads/Satispay
 cd ~/Downloads && find . -name ".DS_Store" -delete
 zip -r Satispay.zip Satispay -x ".*" -x "__MACOSX"
